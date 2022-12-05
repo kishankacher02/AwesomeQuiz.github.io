e218f29fd5ddbe8717f4e0d590d034f31f15deca
kishan kacher <119491278+kishankacher02@users.noreply.github.com>
2022-12-05 23:21:33 +0530

    done

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is my favourite thing?",
+    answer: "kissing you",
+    options: [
+      "you",
+      "going gym",
+      "kissing you",
+      "momos"
+    ]
+  },
+    {
+    numb: 2,
+    question: "i like the most in your body?",
+    answer: "your lips",
+    options: [
+      "your boobs",
+      "your eyes",
+      "your body",
+      "your lips"
+    ]
+  },
+    {
+    numb: 3,
+    question: "our first conversation in instagram?",
+    answer: "7/8/2020",
+    options: [
+      "7/8/2020",
+      "10/8/2020",
+      "3/8/2020",
+      "6/8/2020"
+    ]
+  },
+    {
+    numb: 4,
+    question: "our first video call?",
+    answer: "27/11/2020",
+    options: [
+      "20/11/2020",
+      "27/11/2020",
+      "29/11/2020",
+      "04/12/2020"
+    ]
+  },
+    {
+    numb: 5,
+    question: "i call you tha most?",
+    answer: "chutiya",
+    options: [
+      "my love",
+      "sweetheart",
+      "chutiya",
+      "sweetie"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
